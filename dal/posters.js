f73893d10ad5968e24bb59f7bfab00bb4a3568c9
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-02 14:07:16 +0000

    Added data access layer for posters and refactored code in posters routes

---
+++ dal/posters.js
@@ -0,0 +1,109 @@
+// Require the Poster model
+const { Poster, MediaProperty, Tag } = require('../models');
+
+// TODO:
+// getAll : get all the posters
+// getMediaProperties :  retrieve all the media properties
+// getTags : retrieve all the tags
+// addPoster : add a new poster to the database
+// findPoster : retrieve a poster by id
+
+const getAll = async function () {
+  const posters = await Poster.collection().fetch({
+    withRelated: ['mediaProperty', 'tags']
+  });
+
+  return posters;
+}
+
+const getMediaProperties = async function () {
+  const mediaProperties = await MediaProperty.fetchAll().map(property => [property.get('id'), property.get('name')]);
+
+  return mediaProperties;
+}
+
+const getTags = async function () {
+  const tags = await Tag.fetchAll().map(tag => [tag.get('id'), tag.get('name')]);
+
+  return tags;
+}
+
+const findPoster = async function (posterId) {
+  const poster = Poster.where({
+    id: parseInt(posterId)
+  }).fetch({
+    require: true,
+    withRelated: ['mediaProperty', 'tags']
+  });
+
+  return poster;
+}
+
+const addPoster = async function (data) {
+  // Create new instance of Poster model (represents a row in posters table)
+  const poster = new Poster();
+
+  // Separate poster data and tags
+  let { tags, ...posterData } = data;
+
+  // Populate new poster with data
+  poster.set(posterData);
+
+  // Save instance of Poster model
+  await poster.save();
+
+  // Add m:m relationship with tags (if applicable)
+  if (tags) {
+    await poster.tags().attach(tags.split(','));
+  }
+
+  return poster;
+}
+
+const updatePoster = async function (posterId, data) {
+  // Find poster by id
+  const poster = await findPoster(posterId);
+
+  // Separate poster data and tags
+  let { tags, ...posterData } = data;
+
+  // Populate poster with new data
+  poster.set(posterData);
+
+  // Save instance of Poster model
+  await poster.save();
+
+  // Update m:m relationship with tags
+  let tagIds = tags.split(',').map(id => parseInt(id)); // Split all selected tags into an array and map it to int (default is string)
+  let existingTagIds = await poster.related('tags').pluck('id'); // Get the id of all tags that are currently associated with the poster
+
+  // Remove existing tags that are no longer selected
+  let toRemove = existingTagIds.filter(id => !tagIds.includes(id)); // Get the id of all existing tags that are no longer selected
+  await poster.tags().detach(toRemove);
+
+  // Add all tags selected (duplicates are handled by bookshelf)
+  await poster.tags().attach(tagIds);
+
+  return true; // Indicate success
+}
+
+const deletePoster = async function(posterId) {
+  // Find poster by id
+  const poster = await findPoster(posterId);
+
+  // Delete poster
+  await poster.destroy();
+
+  return true; // Indicate success
+}
+
+
+module.exports = {
+  getAll,
+  getMediaProperties,
+  getTags,
+  findPoster,
+  addPoster,
+  updatePoster,
+  deletePoster
+};
--- routes/posters.js
+++ routes/posters.js
@@ -8,6 +8,9 @@ const { Poster, MediaProperty, Tag } = require('../models');
 // Import middleware
 const { checkIfAuthenticated } = require('../middlewares');
 
+// Import DAL (Data Access Layer)
+const dataLayer = require('../dal/posters');
+
 // ---------------------------------------
 // READ
 // router.get('/', async function (req, res) {
@@ -25,12 +28,14 @@ const { checkIfAuthenticated } = require('../middlewares');
 // READ (WITH SEARCH ENGINE)
 router.get('/', async function (req, res) {
   // Get all media properties
-  const mediaProperties = await MediaProperty.fetchAll().map(property => [property.get('id'), property.get('name')]);
+  // const mediaProperties = await MediaProperty.fetchAll().map(property => [property.get('id'), property.get('name')]);
+  const mediaProperties = await dataLayer.getMediaProperties();
 
   mediaProperties.unshift([0, '--- Any media property ---']); // Append an option to front of array to select all media properties
 
   // Get all tags
-  const tags = await Tag.fetchAll().map(tag => [tag.get('id'), tag.get('name')]);
+  // const tags = await Tag.fetchAll().map(tag => [tag.get('id'), tag.get('name')]);
+  const tags = await dataLayer.getTags();
 
   // Create search engine
   const searchForm = createSearchForm(mediaProperties, tags);
@@ -87,9 +92,10 @@ router.get('/', async function (req, res) {
     },
     empty: async function (form) {
       // Fetch all the posters
-      let posters = await query.fetch({
-        withRelated: ['mediaProperty', 'tags'] // Name of the relationship (function name in model)
-      });
+      // let posters = await query.fetch({
+      //   withRelated: ['mediaProperty', 'tags'] // Name of the relationship (function name in model)
+      // });
+      const posters = await dataLayer.getAll();
 
       res.render('posters/index', {
         posters: posters.toJSON(),
@@ -98,9 +104,10 @@ router.get('/', async function (req, res) {
     },
     error: async function (form) {
       // Fetch all the posters
-      let posters = await query.fetch({
-        withRelated: ['mediaProperty', 'tags'] // Name of the relationship (function name in model)
-      });
+      // let posters = await query.fetch({
+      //   withRelated: ['mediaProperty', 'tags'] // Name of the relationship (function name in model)
+      // });
+      const posters = await dataLayer.getAll();
 
       res.render('posters/index', {
         posters: posters.toJSON(),
@@ -115,10 +122,12 @@ router.get('/', async function (req, res) {
 // CREATE
 router.get('/create', checkIfAuthenticated, async function (req, res) {
   // Get all media properties
-  const mediaProperties = await MediaProperty.fetchAll().map(property => [property.get('id'), property.get('name')]);
+  // const mediaProperties = await MediaProperty.fetchAll().map(property => [property.get('id'), property.get('name')]);
+  const mediaProperties = await dataLayer.getMediaProperties();
 
   // Get all tags
-  const tags = await Tag.fetchAll().map(tag => [tag.get('id'), tag.get('name')]);
+  // const tags = await Tag.fetchAll().map(tag => [tag.get('id'), tag.get('name')]);
+  const tags = await dataLayer.getTags();
 
   const posterForm = createPosterForm(mediaProperties, tags);
   res.render('posters/create', {
@@ -132,34 +141,38 @@ router.get('/create', checkIfAuthenticated, async function (req, res) {
 
 router.post('/create', checkIfAuthenticated, async function (req, res) {
   // Get all media properties
-  const mediaProperties = await MediaProperty.fetchAll().map(property => [property.get('id'), property.get('name')]);
+  // const mediaProperties = await MediaProperty.fetchAll().map(property => [property.get('id'), property.get('name')]);
+  const mediaProperties = await dataLayer.getMediaProperties();
 
   // Get all tags
-  const tags = await Tag.fetchAll().map(tag => [tag.get('id'), tag.get('name')]);
+  // const tags = await Tag.fetchAll().map(tag => [tag.get('id'), tag.get('name')]);
+  const tags = await dataLayer.getTags();
 
   const posterForm = createPosterForm(mediaProperties, tags);
   posterForm.handle(req, {
     // Handle success
     success: async function (form) {
-      const poster = new Poster(); // represents one row in the posters table
-      poster.set('title', form.data.title);
-      poster.set('cost', form.data.cost);
-      poster.set('description', form.data.description);
-      poster.set('date', form.data.date);
-      poster.set('stock', form.data.stock);
-      poster.set('height', form.data.height);
-      poster.set('width', form.data.width);
-      // Add foreign key
-      poster.set('media_property_id', form.data.media_property_id);
+      // const poster = new Poster(); // represents one row in the posters table
+      // poster.set('title', form.data.title);
+      // poster.set('cost', form.data.cost);
+      // poster.set('description', form.data.description);
+      // poster.set('date', form.data.date);
+      // poster.set('stock', form.data.stock);
+      // poster.set('height', form.data.height);
+      // poster.set('width', form.data.width);
+      // // Add foreign key
+      // poster.set('media_property_id', form.data.media_property_id);
 
-      // Add image_url
-      poster.set('image_url', form.data.image_url);
-      await poster.save();
+      // // Add image_url
+      // poster.set('image_url', form.data.image_url);
+      // await poster.save();
 
-      // Save m:m relationship with tags
-      if (form.data.tags) {
-        await poster.tags().attach(form.data.tags.split(','));
-      }
+      // // Save m:m relationship with tags
+      // if (form.data.tags) {
+      //   await poster.tags().attach(form.data.tags.split(','));
+      // }
+
+      const poster = await dataLayer.addPoster(form.data);
 
       // Add flash message
       req.flash("success_messages", `New poster ${poster.get('title')} has been created`);
@@ -193,19 +206,22 @@ router.post('/create', checkIfAuthenticated, async function (req, res) {
 // UPDATE
 router.get('/:poster_id/update', checkIfAuthenticated, async function (req, res) {
   // Get poster to be updated
-  const posterId = req.params.poster_id;
-  const poster = await Poster.where({
-    id: posterId
-  }).fetch({
-    require: true,
-    withRelated: ['mediaProperty', 'tags']
-  })
+  // const posterId = req.params.poster_id;
+  // const poster = await Poster.where({
+  //   id: posterId
+  // }).fetch({
+  //   require: true,
+  //   withRelated: ['mediaProperty', 'tags']
+  // })
+  const poster = await dataLayer.findPoster(req.params.poster_id);
 
   // Get all media properties
-  const mediaProperties = await MediaProperty.fetchAll().map(property => [property.get('id'), property.get('name')]);
+  // const mediaProperties = await MediaProperty.fetchAll().map(property => [property.get('id'), property.get('name')]);
+  const mediaProperties = await dataLayer.getMediaProperties();
 
   // Get all tags
-  const tags = await Tag.fetchAll().map(tag => [tag.get('id'), tag.get('name')]);
+  // const tags = await Tag.fetchAll().map(tag => [tag.get('id'), tag.get('name')]);
+  const tags = await dataLayer.getTags();
 
   // Create poster form
   const posterForm = createPosterForm(mediaProperties, tags);
@@ -238,38 +254,42 @@ router.get('/:poster_id/update', checkIfAuthenticated, async function (req, res)
 
 router.post('/:poster_id/update', checkIfAuthenticated, async function (req, res) {
   // Get the poster to be updated
-  const poster = await Poster.where({
-    id: req.params.poster_id
-  }).fetch({
-    require: true,
-    withRelated: ['mediaProperty', 'tags']
-  });
+  // const poster = await Poster.where({
+  //   id: req.params.poster_id
+  // }).fetch({
+  //   require: true,
+  //   withRelated: ['mediaProperty', 'tags']
+  // });
+  const poster = await dataLayer.findPoster(req.params.poster_id);
 
   // Get all media properties
-  const mediaProperties = await MediaProperty.fetchAll().map(property => [property.get('id'), property.get('name')]);
+  // const mediaProperties = await MediaProperty.fetchAll().map(property => [property.get('id'), property.get('name')]);
+  const mediaProperties = await dataLayer.getMediaProperties();
 
   // Get all tags
-  const tags = await Tag.fetchAll().map(tag => [tag.get('id'), tag.get('name')]);
+  // const tags = await Tag.fetchAll().map(tag => [tag.get('id'), tag.get('name')]);
+  const tags = await dataLayer.getTags();
 
   // Process form
   const posterForm = createPosterForm(mediaProperties, tags);
   posterForm.handle(req, {
     // Handle success
     success: async function (form) {
-      let { tags, ...posterData } = form.data;
-      poster.set(posterData); // This works because the name of the column in posters table matches that of the form field name
-      await poster.save();
+      // let { tags, ...posterData } = form.data;
+      // poster.set(posterData); // This works because the name of the column in posters table matches that of the form field name
+      // await poster.save();
 
-      // Update tags
-      let tagIds = tags.split(',').map(id => parseInt(id)); // To convert id to int (default is string)
-      let existingTagIds = await poster.related('tags').pluck('id'); // Get ids of all tags that are currently in poster
+      // // Update tags
+      // let tagIds = tags.split(',').map(id => parseInt(id)); // To convert id to int (default is string)
+      // let existingTagIds = await poster.related('tags').pluck('id'); // Get ids of all tags that are currently in poster
 
-      // Remove all tags that are not selected anymore
-      let toRemove = existingTagIds.filter(id => !tagIds.includes(id));
-      await poster.tags().detach(toRemove);
+      // // Remove all tags that are not selected anymore
+      // let toRemove = existingTagIds.filter(id => !tagIds.includes(id));
+      // await poster.tags().detach(toRemove);
 
-      // Add in all the tags selected in the form
-      await poster.tags().attach(tagIds);
+      // // Add in all the tags selected in the form
+      // await poster.tags().attach(tagIds);
+      await dataLayer.updatePoster(req.params.poster_id, form.data);
 
       // Add flash message
       req.flash('success_messages', `Poster ${poster.get('title')} successfully updated`);
@@ -296,11 +316,12 @@ router.post('/:poster_id/update', checkIfAuthenticated, async function (req, res
 // DELETE
 router.get('/:poster_id/delete', checkIfAuthenticated, async function (req, res) {
   // Get poster to be deleted
-  const poster = await Poster.where({
-    id: req.params.poster_id
-  }).fetch({
-    require: true
-  });
+  // const poster = await Poster.where({
+  //   id: req.params.poster_id
+  // }).fetch({
+  //   require: true
+  // });
+  const poster = await dataLayer.findPoster(req.params.poster_id);
 
   res.render('posters/delete', {
     poster: poster.toJSON()
@@ -309,14 +330,16 @@ router.get('/:poster_id/delete', checkIfAuthenticated, async function (req, res)
 
 router.post('/:poster_id/delete', checkIfAuthenticated, async function (req, res) {
   // Get the poster to be deleted
-  const poster = await Poster.where({
-    id: req.params.poster_id
-  }).fetch({
-    require: true
-  });
+  // const poster = await Poster.where({
+  //   id: req.params.poster_id
+  // }).fetch({
+  //   require: true
+  // });
+  const poster = await dataLayer.findPoster(req.params.poster_id);
 
   // Delete poster
-  await poster.destroy();
+  // await poster.destroy();
+  await dataLayer.deletePoster(req.params.poster_id);
 
   // Add flash message
   req.flash('success_messages', `Poster ${poster.get('title')} successfully deleted`);
