b3330328145337e4fbdb88971f7c4503d6f7bead
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-02 14:54:18 +0000

    CAdded cart_items table and model

--- models/index.js
+++ models/index.js
@@ -3,29 +3,39 @@ const bookshelf = require('../bookshelf');
 const Poster = bookshelf.model('Poster', {
   tableName: 'posters',
   mediaProperty: function () {
-    return this.belongsTo('MediaProperty')
+    return this.belongsTo('MediaProperty');
   },
   tags: function () {
-    return this.belongsToMany('Tag')
+    return this.belongsToMany('Tag');
   }
-})
+});
 
 const MediaProperty = bookshelf.model('MediaProperty', {
   tableName: 'media_properties',
   posters: function () {
     return this.hasMany('Poster');
   }
-})
+});
 
 const Tag = bookshelf.model('Tag', {
   tableName: 'tags',
   posters: function () {
     return this.belongsToMany('Poster');
   }
-})
+});
 
 const User = bookshelf.model('User', {
   tableName: 'users'
-})
+});
 
-module.exports = { Poster, MediaProperty, Tag, User }
+const CartItem = bookshelf.model('CartItem', {
+  tableName: 'cart_items',
+  poster: function () {
+    return this.belongsTo('Poster');
+  },
+  user: function () {
+    return this.belongsTo('User');
+  }
+});
+
+module.exports = { Poster, MediaProperty, Tag, User, CartItem };
