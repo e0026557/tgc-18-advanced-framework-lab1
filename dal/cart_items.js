af467ba635ad1cef364eb4871adea517ff1237c8
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-03 07:13:49 +0000

    Added getCart and getCartItem functions in DAL for cart_items

--- dal/cart_items.js
+++ dal/cart_items.js
@@ -1,1 +1,30 @@
 // TODO
+const { CartItem } = require('../models');
+
+const getCart = async function (userId) {
+  // Get all items in user's shopping cart
+  return await CartItem.collection().where({
+    user_id: userId
+  }).fetch({
+    require: false,
+    withRelated: ['poster', 'poster.mediaProperty']
+  });
+}
+
+const getCartItem = async function (userId, posterId) {
+  // Get a cart item in user's shopping cart
+  return await CartItem.collection.where({
+    user_id: userId,
+    poster_id: posterId
+  }).fetch({
+    require: false
+  });
+}
+
+// TODO: createCartItem
+
+// TODO: removeFromCart
+
+// TODO: updateQuantity
+
+module.exports = { getCart, getCartItem };
